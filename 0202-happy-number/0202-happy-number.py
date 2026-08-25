3070239deb46bcfd1c2059cf53f3dc94732b82e2
Anuthapa-code <anuthapa7026@gmail.com>
2026-08-25 22:20:23 +0530

    Time: 3 ms (33%), Space: 19.3 MB (25.12%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def fun(n):
+            sum = 0
+            while n > 0:
+                d = n%10
+                n =n//10
+                sum = sum+d*d
+            return sum
+
+
+        slow = n
+        fast = n
+        while fast!=1:
+            slow = fun(slow)
+            fast = fun(fun(fast))
+
+            if slow == fast and slow != 1:
+                return False
+
+        return True       
